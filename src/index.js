acc0dc60ea1f70855380236602651ade9d9a37aa
Evgeny S <evgen7e@yandex.ru>
2022-01-04 17:31:04 +0300

    feat: function toReadable ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function toReadable (number) {
-  
+    const dict = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+        100: 'hundred',
+    }
+
+    if (number < 20) {
+        return dict[number];
+    } else {
+        let numArr = number.toString().split('');
+        if (numArr.length == 2) {
+            return dict[Number(numArr[0]) * 10] + (Number(numArr[1]) != 0 ? dict[Number(numArr[1])] : '');
+        } else if (numArr.length == 3) {
+            return dict[Number(numArr[0])] + dict[100] + (Number(numArr[1]) != 0 ? dict[Number(numArr[1]) * 10] : '') + (Number(numArr[2]) != 0 ? dict[Number(numArr[2])] : '');
+        } else {
+            return 'Numbers greater than 999 are not supported';
+        }
+    }
 }
